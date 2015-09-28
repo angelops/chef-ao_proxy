f075ee9cab12d5433ddda72c4f91b26309b056d3
Justin Alan Ryan <juryan@ziprealty.com>
2015-09-28 11:14:40 -0700

    basics: include squid cookbook, set network to 10/8.

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+node.default['squid']['network'] = '10.0.0.0/8'
+
--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,6 @@ license          'Apache 2.0'
 description      'Installs/Configures zip_proxy'
 long_description 'Installs/Configures zip_proxy'
 version          '0.1.0'
+
+depends 'squid'
+
