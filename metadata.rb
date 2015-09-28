169c3fc3455697c7bd7fd7a67d75158499b2ba71
Justin Alan Ryan <juryan@ziprealty.com>
2015-09-28 11:12:45 -0700

    initial cookbook gen.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'zip_proxy'
+maintainer       'Justin Alan Ryan (ZipRealty / Realogy)'
+maintainer_email 'juryan@ziprealty.com'
+license          'Apache 2.0'
+description      'Installs/Configures zip_proxy'
+long_description 'Installs/Configures zip_proxy'
+version          '0.1.0'
